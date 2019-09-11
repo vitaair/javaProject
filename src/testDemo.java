baca6e7db516ff0f9234957db5a280d823f6300d
vitaair <vitaair@outlook.com>
2019-09-12 02:18:50 +0800

    the Sencond testDemo!

--- src/testDemo.java
+++ src/testDemo.java
@@ -1,5 +1,6 @@
 public class testDemo {
     public static void main(String[] args) {
-        System.out.println("这是一个 test demo");
+        System.out.println("这是一个  test demo");
+        System.out.println("这是另一个 test demo2");
     }
 }
